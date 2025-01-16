4f893de3ce32ec4d0a44d2eb894da38137491c10
Goonn9 <goncamvi99@gmail.com>
2025-01-16 21:33:40 +0100

    bbdd buena

--- ahorro.sql
+++ ahorro.sql
@@ -1,1 +1,26 @@
-SELECT * FROM ahorro.categorias;
+CREATE DATABASE `ahorro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE TABLE `categorias` (
+  `idCategorias` int NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  `usuarioId` int DEFAULT NULL,
+  PRIMARY KEY (`idCategorias`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `transacciones` (
+  `idTransacciones` int NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `categoriaId` int DEFAULT NULL,
+  `usuarioId` int DEFAULT NULL,
+  `cantidad` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`idTransacciones`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `usuarios` (
+  `idUsuarios` int NOT NULL,
+  `nombreCompleto` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `correo` varchar(45) DEFAULT NULL,
+  `rol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idUsuarios`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

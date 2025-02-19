e84d2f4dd9e9b3a28199be842b56ffad1e7d9963
Goonn9 <goncamvi99@gmail.com>
2025-02-20 00:28:12 +0100

    bbdd bien

--- ahorro.sql
+++ ahorro.sql
@@ -1,26 +1,27 @@
 CREATE DATABASE `ahorro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+use ahorro;
 CREATE TABLE `categorias` (
-  `idCategorias` int NOT NULL,
+  `idCategoria` int NOT NULL,
   `nombre` varchar(45) DEFAULT NULL,
   `tipo` varchar(45) DEFAULT NULL,
   `usuarioId` int DEFAULT NULL,
-  PRIMARY KEY (`idCategorias`)
+  PRIMARY KEY (`idCategoria`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `transacciones` (
-  `idTransacciones` int NOT NULL,
+  `idTransaccion` int NOT NULL,
   `fecha` date DEFAULT NULL,
   `categoriaId` int DEFAULT NULL,
   `usuarioId` int DEFAULT NULL,
   `cantidad` decimal(10,0) DEFAULT NULL,
-  PRIMARY KEY (`idTransacciones`)
+  PRIMARY KEY (`idTransaccion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `usuarios` (
-  `idUsuarios` int NOT NULL,
+  `idUsuario` int NOT NULL,
   `nombreCompleto` varchar(45) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
   `correo` varchar(45) DEFAULT NULL,
   `rol` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idUsuarios`)
+  PRIMARY KEY (`idUsuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

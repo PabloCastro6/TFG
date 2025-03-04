4b8d57d7c81f5d1f729f407eddbb59b89b99fd16
Goonn9 <goncamvi99@gmail.com>
2025-03-04 12:03:38 +0100

    dejando solo una bbdd y añadido pequeña parte en java

--- VamonosAhorrandoBackend/src/main/java/com/proyecto/repository/CategoriaRepository.java
+++ VamonosAhorrandoBackend/src/main/java/com/proyecto/repository/CategoriaRepository.java
@@ -1,5 +1,7 @@
 package com.proyecto.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import com.proyecto.model.Categoria;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	List<Categoria> findByCategoriaIdCategoria(Long categoriaId);
+
 }
--- VamonosAhorrandoBackend/src/main/java/com/proyecto/repository/UsuarioRepository.java
+++ VamonosAhorrandoBackend/src/main/java/com/proyecto/repository/UsuarioRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.proyecto.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
     Usuario findByCorreo(String correo);
 }
